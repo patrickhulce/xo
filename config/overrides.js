f00abc7cdf6688bb5d8a70e452c62fb1fef05be4
Patrick Hulce <patrick.hulce@gmail.com>
2016-11-30 15:04:56 -0800

    Fix overrides.js

--- config/overrides.js
+++ config/overrides.js
@@ -1,9 +1,9 @@
 'use strict';
 module.exports = {
-  space: 2,
   rules: {
+		indent: ['error', 2],
     complexity: ['warn', 10],
-    'xo/filename-case': 0,
+    'unicorn/filename-case': 0,
     'no-else-return': 0,
     'comma-dangle': ['error', 'always-multiline'],
     'dot-location': ['error', 'object']
