a26f6366a2ef6f37e3e86a8f92c67855f4663bec
Patrick Hulce <patrick.hulce@gmail.com>
2016-11-30 14:31:01 -0800

    Custom overrides

--- config/overrides.js
+++ config/overrides.js
@@ -1,4 +1,11 @@
 'use strict';
 module.exports = {
-	// put rule overrides here
+  space: 2,
+  rules: {
+    complexity: ['warn', 10],
+    'xo/filename-case': 0,
+    'no-else-return': 0,
+    'comma-dangle': ['error', 'always-multiline'],
+    'dot-location': ['error', 'object']
+  }
 };
