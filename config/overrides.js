304d1317361a9a8ff6ef3dc06df6e8fe9c6a0760
Patrick Hulce <patrick.hulce@gmail.com>
2016-12-04 12:10:39 -0800

    Specific indentation, enforce no semi-colons

--- config/overrides.js
+++ config/overrides.js
@@ -1,11 +1,16 @@
 'use strict';
 module.exports = {
   rules: {
-    indent: ['error', 2],
+    semi: ['error', 'never'],
+    indent: ['error', 2, {
+      SwitchCase: 1,
+      MemberExpression: 1,
+      CallExpression: {arguments: 1}
+    }],
     complexity: ['warn', 10],
-    'unicorn/filename-case': 0,
+    'unicorn/filename-case': ['warn', {case: 'kebabCase'}],
     'no-else-return': 0,
     'comma-dangle': ['error', 'always-multiline'],
-    'dot-location': ['error', 'object']
+    'dot-location': ['error', 'property']
   }
 };
